e588873580a8ef36ca09bc283bfda0025202a628
dalexach <dalexach@gmail.com>
2020-01-05 05:39:22 +0000

    Fabric script that generates a.tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Script to generate a .tgz archive
+"""
+
+
+def do_pack():
+    """
+        Funtion that creates a .tgz
+    """
+
+    from datetime import datetime
+    from fabric.operations import local
+
+    date_now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    path = "./versions/web_static_{}".format(date_now)
+    local("mkdir -p versions")
+    create = local("tar -cvzf {} web_static".format(path))
+
+    if create.succeded:
+        return path
+    else:
+        return None
+
+if __name__ == "__main__":
+    do_pack()
