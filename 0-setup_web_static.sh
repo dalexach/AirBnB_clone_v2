71ba257e97420c79675980987bb83c763eacd8b8
dalexach <dalexach@gmail.com>
2020-01-05 05:09:28 +0000

    add the direction to sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -24,7 +24,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 
 # Update the Nginx configuration to serve the content of web_static/current/ to hbnb_static
-sudo sed -i "/# Only/ i location /hbnb_static {\nalias /data/web_static/current;\n}"
+sudo sed -i "/# Only/ i location /hbnb_static {\nalias /data/web_static/current;\n}" /etc/nginx/sites-enabled/default
 
 # Restart the service
 sudo service nginx restart
