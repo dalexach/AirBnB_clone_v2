266cdc349dc529c9cac7499d289316779af0c880
dalexach <dalexach@gmail.com>
2020-01-21 21:18:05 +0000

    Replace underscore _ symbols with a space

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    Starts a Flask web application to listen on 0.0.0.0:5000
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """
+        Display Hello HBNB!
+    """
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+        Route /hbnb: display “HBNB”
+    """
+    return ("HBNB")
+
+
+@app.route('/c/<text>')
+def c(text):
+    """
+        Replace underscore _ symbols with a space
+    """
+    return ("C {}".format(text.replace('_', ' ')))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
