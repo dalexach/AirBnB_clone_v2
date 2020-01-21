e42a0319fd00192451531ae473e48237d388ac95
dalexach <dalexach@gmail.com>
2020-01-21 16:59:37 +0000

    First steps

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Starts a Flask web application
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """
+        Display Hello HBNB!
+    """
+    return ('Hello HBNB!')
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)
