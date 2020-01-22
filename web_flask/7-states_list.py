177d114255c9f9621d0a25d5075a58fd5ae3598d
dalexach <dalexach@gmail.com>
2020-01-22 17:11:05 +0000

    Upload task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    Starts a Flask web application to listen on 0.0.0.0:5000
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """
+        List states in a jimja template
+    """
+    all_states = storage.all("State")
+    return render_template('7-states_list.html', states=all_states)
+
+
+@app.teardown_appcontext('/hbnb')
+def tear_down(self):
+    """
+        Closing the storage
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
