7a417bfb451f909679b148c22b66681b8bc5b549
dalexach <dalexach@gmail.com>
2020-01-22 16:59:52 +0000

    everything back

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -89,15 +89,15 @@ class DBStorage:
     def reload(self):
         """Reload the session
         """
-        # Base.metadata.create_all(self.__engine)
-        # self.__session = scoped_session(sessionmaker(
-        #     bind=self.__engine,
-        #     expire_on_commit=False
-        # ))
         Base.metadata.create_all(self.__engine)
-        sess = sessionmaker(bind=self.__engine, expore_on_commit=False)
-        Session = scoped_session(sess)
-        self.__session = Session()
+        self.__session = scoped_session(sessionmaker(
+            bind=self.__engine,
+            expire_on_commit=False
+        ))
+        # Base.metadata.create_all(self.__engine)
+        # sess = sessionmaker(bind=self.__engine, expore_on_commit=False)
+        # Session = scoped_session(sess)
+        # self.__session = Session()
 
     def close(self):
         """
