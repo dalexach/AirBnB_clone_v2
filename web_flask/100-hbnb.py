bda89f771d66b85899b3976379932fe4408e17b8
dalexach <dalexach@gmail.com>
2020-01-23 00:02:35 +0000

    Upload the advanced

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    Starts a Flask web application to listen on 0.0.0.0:5000
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/hbnb')
+def display_site():
+    """
+    display HBNB HTML page
+    """
+    states = storage.all(classes["State"]).values()
+    amenities = storage.all(classes["Amenity"]).values()
+    places = storage.all(classes["Place"]).values()
+    return (render_template('100-hbnb.html', states=states,
+                            amenities=amenities, places=places))
+
+
+@app.teardown_appcontext
+def teardown_app(exception):
+    """
+        Closing the storage
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
